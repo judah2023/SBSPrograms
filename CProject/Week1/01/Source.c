bcd40d74d04829441a2962a131f1094f00063bca
1 <124682191+judah2023@users.noreply.github.com>
2023-04-12 21:47:33 +0900

    (2023.04.12) C프로그램 파일
    변수, 상수, 선언, 초기화

--- CProject/Week1/01/Source.c
+++ CProject/Week1/01/Source.c
@@ -1,18 +1,25 @@
 ﻿#include <stdio.h>	// 기본 입출력 헤더 파일
 
-void main()			// 프로그램의 시작점
+void main()			// 프로그램의 시작점, 프로그렘에 하나만 존재해야 한다.
 {
-	// 변수란?
-	// 데이터를 저장할 수 있는 메모리 공간을
-	// 생성하는 것 입니다.
+#pragma region Operator
+	int result = (1 == 2) ? 10 - 25 : 10 + 25;
 
-	// 자료형이란?
-	// 데이터를 저장하기 위한 데이터의 형태를
-	// 정해주는 것입니다.
+	// 1. num1 변수 = B 변수 + C 변수
+	// 2. num2 변수 = E 변수 - F 상수
+	// 3. num3 변수 = 리터럴 * B 변수
+	// 4. num4 변수 = 리터럴 / F 상수
+	// 5. num5 변수 = 리터럴 + 리터럴
+
+	int num1, num2, num3, num4, num5;
+	int B = 10, C = 20, E = 30;
+	const int F = 40;
 	
-	// 자료형 변수의 이름
-	// ;(세미콜론) : 한 문장의 끝을 알려주는 기호입니다.
-	int		nData;
-
-	printf("안녕하세요\n");
+	num1 = B + C;
+	num2 = E - F;
+	num3 = 50 * B;
+	num4 = 60 / F;
+	num5 = 70 + 80;
+#pragma endregion
+	printf("%d\n", result);
 }
