87d9258f2c59588f94dad04063ed88f8bd2bb0f3
1 <124682191+judah2023@users.noreply.github.com>
2023-04-13 21:46:26 +0900

    (2023.04.13) 비트
    형식 지정자, 비트 연산

--- CProject/Week1/01/(2023.04.12)Second(WED)_NAMING.h
+++ CProject/Week1/01/(2023.04.12)Second(WED)_NAMING.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 
+#include <stdio.h>	// 기본 입출력 헤더 파일
 
+void main_230412()			// 프로그램의 시작점, 프로그렘에 하나만 존재해야 한다.
+{
 #pragma region VariableRule
 	// 1. 예약어를 사용할 수 없다.
 	// int int = 100;			(X)
@@ -40,3 +43,24 @@
 	// 심볼릭 상수 : 메모리 공간을 가지고 있는 상수
 	// 리터럴 상수 : 메모리 공간을 가지고 있지 않은 상수
 #pragma endregion
+#pragma region Operator
+	int result = (1 == 2) ? 10 - 25 : 10 + 25;
+
+	// 1. num1 변수 = B 변수 + C 변수
+	// 2. num2 변수 = E 변수 - F 상수
+	// 3. num3 변수 = 리터럴 * B 변수
+	// 4. num4 변수 = 리터럴 / F 상수
+	// 5. num5 변수 = 리터럴 + 리터럴
+
+	int num1, num2, num3, num4, num5;
+	int B = 10, C = 20, E = 30;
+	const int F = 40;
+
+	num1 = B + C;
+	num2 = E - F;
+	num3 = 50 * B;
+	num4 = 60 / F;
+	num5 = 70 + 80;
+#pragma endregion
+	printf("%d\n", result);
+}
