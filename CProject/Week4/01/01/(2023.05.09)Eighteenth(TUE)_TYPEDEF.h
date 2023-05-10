f22b9e7c4bf1023b75781b30f25fbdb57673b66d
pain103190@gmail.com <judah2013@naver.com>
2023-05-10 21:47:08 +0900

    (2023.05.10) 자기 참조 구조체
    삼항 연산자, 한 줄 입력

---
+++ CProject/Week4/01/01/(2023.05.09)Eighteenth(TUE)_TYPEDEF.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+void Function()
+{
+	printf("Function() 실행중...\n");
+}
+
+void DataList()
+{
+	printf("DataList() 실행중...\n");
+}
+
+int Damage(int x)
+{
+	return x;
+}
+
+void Sort(void (*fPtr)())
+{
+	fPtr();
+}
+
+#pragma region typedef
+
+/*
+
+typedef unsigned int UINT;
+
+typedef struct Player
+{
+	int x;
+	int y;
+	char name[10];
+}Player;
+
+*/
+
+#pragma endregion
+
+
+int main()
+{
+#pragma region Function_Pointer
+
+	/*
+
+	// 함수의 주솟값을 저장하고 가리킬 수 있는 변수
+	printf("Function()의 주솟값 : %p\n", Function);
+
+	// type (*name)(parameters)		// 함수 포인터 선언 방법
+	void (*fPtr) ();
+	int (*newFPtr)(int);
+
+	fPtr = Function;
+	newFPtr = Damage;
+
+	fPtr();
+
+	// 함수 포인터는 함수의 반환형과 매개변수의 타입이 일치해야 하므로,
+	// 함수 포인터를 사용하여 동적으로 메모리를 할당할 수 없다.
+	// fPtr = Damage;
+	// printf("fPtr의 값 : %d\n", fPtr(10));		// Error
+
+	printf("newFPtr의 값 : %d\n", newFPtr(10));
+
+	fPtr = DataList;
+	fPtr();
+
+	Sort(Function);
+
+	*/
+
+#pragma endregion
+
+	return 0;
+}
--- CProject/Week4/01/01/Source.c
+++ CProject/Week4/01/01/Source.c
@@ -1,84 +1,112 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
-
-void Function()
-{
-	printf("Function() 실행중...\n");
-}
-
-void DataList()
-{
-	printf("DataList() 실행중...\n");
-}
-
-int Damage(int x)
-{
-	return x;
-}
-
-void Sort(void (*fPtr)())
-{
-	fPtr();
-}
-
-#pragma region typedef
+#pragma region Self_Referential_Structure
 
 /*
 
-typedef unsigned int UINT;
+// 자기자신을 가리키는 구조체 포인터가 
 
-typedef struct Player
+typedef struct _Node
 {
-	int x;
-	int y;
-	char name[10];
-}Player;
+	int data;
+	struct _Node* next;
+}Node;
+
+Node CreateNode(int data)
+{
+	Node newNode;
+	newNode.data = data;
+	newNode.next = NULL;
+
+	return newNode;
+}
+
+void InsertNode(Node* head, Node* newNode, int number)
+{
+	Node* prevNode = head;
+
+	if (number == 0)
+	{
+		newNode->next = head;
+		head = newNode;
+		return;
+	}
+
+	for (int i = 1; i < number - 1; i++)
+	{
+		prevNode = prevNode->next;
+	}
+
+	newNode->next = prevNode->next;
+	prevNode->next = newNode;
+}
 
 */
 
 #pragma endregion
 
-
 int main()
 {
-#pragma region Function_Pointer
-	
+#pragma region Ternary_Operator
+
 	/*
 
-	// 함수의 주솟값을 저장하고 가리킬 수 있는 변수
-	printf("Function()의 주솟값 : %p\n", Function);
+	printf("%d\n\n", 1 < 0 ? 100 : // ->
+					 2 < 1 ? 200 : // ->
+					 3 < 2 ? 300 : 400);
 
-	// type (*name)(parameters)		// 함수 포인터 선언 방법
-	void (*fPtr) ();
-	int (*newFPtr)(int);
-	
-	fPtr = Function;
-	newFPtr = Damage;
+	int data = 0;
 
-	fPtr();
-
-	// 함수 포인터는 함수의 반환형과 매개변수의 타입이 일치해야 하므로,
-	// 함수 포인터를 사용하여 동적으로 메모리를 할당할 수 없다.
-	// fPtr = Damage;
-	// printf("fPtr의 값 : %d\n", fPtr(10));		// Error
-
-	printf("newFPtr의 값 : %d\n", newFPtr(10));
-
-	fPtr = DataList;
-	fPtr();
-
-	Sort(Function);
+	//		  조건		참	  거짓
+	data = (10 != 10) ? 100 : 999;
+	printf("%d\n", data);
 	
 	*/
 
 #pragma endregion
 
+	/*
+	
+	char string[15] = { 0 };
+
+	scanf_s("%[^\n]s", string, 15);
+
+	printf("string : %s\n", string);
+
+	*/
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* github - Branch
 * A, B, C 개발자가 협업한다고 가정
@@ -86,4 +114,9 @@ int main()
 * 3 명 모두 Main에서 동시에 작업하면 충돌 가능성이 있음
 * 이런 경우 branch를 사용하여 업무를 분할
 * 각각 작업이 끝나면 자신의 작업물을 Main에 병합(merge)
+*/
+
+/*
+클래스와 구조체(사용 전)의 메모리 위치
+언리얼과 유니티의 차이
 */
