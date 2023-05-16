f84485f666b96896fc48aecefa79f726efda1ba6
pain103190@gmail.com <judah2013@naver.com>
2023-05-16 21:45:55 +0900

    (2023.05.16) 좌표 이동 함수
    strlen, 회문

---
+++ CProject/Month02/Week2/01/01/(2023.05.16) TwentySecond_GOTOXY.h
@@ -0,0 +1,110 @@
+﻿#pragma once
+
+
+int main()
+{
+
+#pragma region Coordinate_Movement
+
+	/*
+
+	int currX = 10, currY = 10;
+	int prevX = 10, prevY = 10;
+
+	while (1)
+	{
+		system("cls");
+		GotoXY(currX, currY);
+		printf("☆");
+
+		prevX = currX;
+		prevY = currX;
+
+		if (GetAsyncKeyState(VK_UP) & 0x0001)
+		{
+			currY--;
+		}
+		if (GetAsyncKeyState(VK_LEFT) & 0x0001)
+		{
+			currX--;
+		}
+		if (GetAsyncKeyState(VK_RIGHT) & 0x0001)
+		{
+			currX++;
+		}
+		if (GetAsyncKeyState(VK_DOWN) & 0x0001)
+		{
+			currY++;
+		}
+
+		if (currY < 0)
+		{
+			currY = 0;
+		}
+		if (currX < 0)
+		{
+			currX = 0;
+		}
+
+		Sleep(16);
+	}
+
+	char key;
+	GotoXY(currX, currY);
+	printf("☆");
+	while (1)
+	{
+		if (_kbhit())
+		{
+			system("cls");
+
+			key = _getch();
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case UP:
+				if (currY > 0)
+				{
+					currY--;
+				}
+				break;
+			case LEFT:
+				if (currX > 0)
+				{
+					currX--;
+				}
+				break;
+			case RIGHT:
+				if (currX < 25)
+				{
+					currX++;
+				}
+				break;
+			case DOWN:
+				if (currY < 25)
+				{
+					currY++;
+				}
+				break;
+			default:
+				break;
+			}
+
+
+			GotoXY(currX, currY);
+			printf("☆");
+		}
+	}
+
+	*/
+
+#pragma endregion
+
+
+	return 0;
+}
--- CProject/Month02/Week2/01/01/Source.c
+++ CProject/Month02/Week2/01/01/Source.c
@@ -11,87 +11,80 @@ enum ARROW_KEYS
 	DOWN	= 80
 };
 
+// 좌표이동 함수
+void GotoXY(int x, int y)
+{
+	COORD position = { x + x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
+
 int main()
 {
-#pragma region _kbhit()
+
+#pragma region strlen()
 
 	/*
 
-	// 동기 : A작업을 하는 중에는 다른 작업이 불가능
-	// 비동기 : A작업이 쉬는 중에 다른 작업이 가능
-	
-	char key = 0;
-	while (1)
-	{
-		if (_kbhit())	// 키보드 입력을 확인하는 함수
-		{
-			
-			key = _getch();	// 방향키 입력 시 224 00이 들어오게 되므로 앞의 값 224를 삭제
-			if (key == -32)
-			{
-				key = _getch();
-			}
+	// 문자열의 크기를 반환하는 함수
+	// 문자열의 끝인 NULL 문자를 포함하지 않는 크기가 나온다
+	char* string = "Count";
 
-			// 224 72 : 11011111 01001000
-			// 스캔 코드
-			// UP		== 72
-			// LEFT		== 75
-			// RIGHT	== 77
-			// DOWN		== 80
-			switch (key)
-			{
-			case UP :
-				printf("UP\n");
-				break;
-			case LEFT :
-				printf("LEFT\n");
-				break;
-			case RIGHT :
-				printf("RIGHT\n");
-				break;
-			case DOWN :
-				printf("DOWN\n");
-				break;
-			default :
-				printf("ERROR!\n");
-				break;
-			}
+	size_t strSize = strlen(string);
 
-			printf("다른 작업 중...\n");
-		}
-	}
-	
+	printf("strSize : %llu\n", strSize);
+
 	*/
 
 #pragma endregion
 
-#pragma region GetAsyncKeyState()
+#pragma region Palindrome
 
 	/*
+
+	char data[101] = { 0 };
+	scanf_s("%s", data, 101);
+	size_t size = strlen(data);
+
+	char* newData = (char*)malloc(sizeof(char) * (size + 1));
+	if (newData == NULL)
+	{
+		printf("ERROR : Failed to Create string!!!\n");
+		return -1;
+	}
+	strcpy_s(newData, size + 1, data);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (newData[i] >= 'a' && newData[i] <= 'z')
+		{
+			newData[i] -= 'a' - 'A';
+		}
+	}
+
+	int flag = 1;
+	for (int i = 0; i < size / 2; i++)
+	{
+		if (newData[size - i -1] != newData[i])
+		{
+			flag = 0;
+			break;
+		}
+	}
+
+	if (flag == 1)
+	{
+		printf("회문\n");
+	}
+	else
+	{
+		printf("회문 아님\n");
+	}
+
+	free(newData);
 	
 	*/
 
-	while (1)
-	{
-		if (GetAsyncKeyState(VK_UP) & 0x0001)
-		{
-			printf("UP\n");
-		}
-		if (GetAsyncKeyState(VK_LEFT) & 0x0001)
-		{
-			printf("LEFT\n");
-		}
-		if (GetAsyncKeyState(VK_RIGHT) & 0x0001)
-		{
-			printf("RIGHT\n");
-		}
-		if (GetAsyncKeyState(VK_DOWN) & 0x0001)
-		{
-			printf("DOWN\n");
-		}
-	}
 #pragma endregion
-
 
 	return 0;
 }
