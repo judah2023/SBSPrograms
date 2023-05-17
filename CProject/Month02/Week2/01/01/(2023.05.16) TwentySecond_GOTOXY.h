5c9bd5d668862d2bb66975eb810b25a1bd69955d
pain103190@gmail.com <judah2013@naver.com>
2023-05-17 21:45:20 +0900

    (2023.05.17) 파일입출력

--- CProject/Month02/Week2/01/01/(2023.05.16) TwentySecond_GOTOXY.h
+++ CProject/Month02/Week2/01/01/(2023.05.16) TwentySecond_GOTOXY.h
@@ -1,5 +1,24 @@
 ﻿#pragma once
 
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+enum ARROW_KEYS
+{
+	UP = 72,
+	LEFT = 75,
+	RIGHT = 77,
+	DOWN = 80
+};
+
+// 좌표이동 함수
+void GotoXY(int x, int y)
+{
+	COORD position = { x + x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 
 int main()
 {
@@ -105,6 +124,70 @@ int main()
 
 #pragma endregion
 
+#pragma region strlen()
+
+	/*
+
+	// 문자열의 크기를 반환하는 함수
+	// 문자열의 끝인 NULL 문자를 포함하지 않는 크기가 나온다
+	char* string = "Count";
+
+	size_t strSize = strlen(string);
+
+	printf("strSize : %llu\n", strSize);
+
+	*/
+
+#pragma endregion
+
+#pragma region Palindrome
+
+	/*
+
+	char data[101] = { 0 };
+	scanf_s("%s", data, 101);
+	size_t size = strlen(data);
+
+	char* newData = (char*)malloc(sizeof(char) * (size + 1));
+	if (newData == NULL)
+	{
+		printf("ERROR : Failed to Create string!!!\n");
+		return -1;
+	}
+	strcpy_s(newData, size + 1, data);
+
+	for (int i = 0; i < size; i++)
+	{
+		if (newData[i] >= 'a' && newData[i] <= 'z')
+		{
+			newData[i] -= 'a' - 'A';
+		}
+	}
+
+	int flag = 1;
+	for (int i = 0; i < size / 2; i++)
+	{
+		if (newData[size - i -1] != newData[i])
+		{
+			flag = 0;
+			break;
+		}
+	}
+
+	if (flag == 1)
+	{
+		printf("회문\n");
+	}
+	else
+	{
+		printf("회문 아님\n");
+	}
+
+	free(newData);
+
+	*/
+
+#pragma endregion
 
 	return 0;
 }
--- CProject/Month02/Week2/01/01/Source.c
+++ CProject/Month02/Week2/01/01/Source.c
@@ -3,87 +3,75 @@
 #include <time.h>
 #include <Windows.h>
 
-enum ARROW_KEYS
-{
-	UP		= 72,
-	LEFT	= 75,
-	RIGHT	= 77,
-	DOWN	= 80
-};
-
-// 좌표이동 함수
-void GotoXY(int x, int y)
-{
-	COORD position = { x + x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
-}
 
 int main()
 {
-
-#pragma region strlen()
-
-	/*
-
-	// 문자열의 크기를 반환하는 함수
-	// 문자열의 끝인 NULL 문자를 포함하지 않는 크기가 나온다
-	char* string = "Count";
-
-	size_t strSize = strlen(string);
-
-	printf("strSize : %llu\n", strSize);
-
-	*/
-
-#pragma endregion
-
-#pragma region Palindrome
+#pragma region File_I/O
 
 	/*
+	FILE* filePtr = NULL;
 
-	char data[101] = { 0 };
-	scanf_s("%s", data, 101);
-	size_t size = strlen(data);
-
-	char* newData = (char*)malloc(sizeof(char) * (size + 1));
-	if (newData == NULL)
+	// fopen("파일의 이름.확장자", 파일모드)
+	// fopen_s(파일 구조체 포인터의 주소, "파일의 이름.확장자", 파일모드)
+	
+	// 파일 모드
+	// w : 쓰기
+	// r : 읽기
+	
+	// 텍스트 파일을 (w)쓰기 모드로 열어준다
+	errno_t err = fopen_s(&filePtr, "DB.txt", "w");
+	if (err)
 	{
-		printf("ERROR : Failed to Create string!!!\n");
+		printf("ERROR : Failed to Open File!!!\n\n");
 		return -1;
-	}
-	strcpy_s(newData, size + 1, data);
-
-	for (int i = 0; i < size; i++)
-	{
-		if (newData[i] >= 'a' && newData[i] <= 'z')
-		{
-			newData[i] -= 'a' - 'A';
-		}
-	}
-
-	int flag = 1;
-	for (int i = 0; i < size / 2; i++)
-	{
-		if (newData[size - i -1] != newData[i])
-		{
-			flag = 0;
-			break;
-		}
-	}
-
-	if (flag == 1)
-	{
-		printf("회문\n");
 	}
 	else
 	{
-		printf("회문 아님\n");
+		printf("=======Success to open file.=======\n");
 	}
 
-	free(newData);
-	
+	fputs("ID : \n", filePtr);
+	fputs("Password : \n", filePtr);
+
+	// 파일 포인터 닫기
+	fclose(filePtr);
+
 	*/
 
+	
+
+	// 파일 읽기
+	// fread(저장할 버퍼 주소, 한번에 읽어 들일 바이트 수, 총 읽어 들일 바이트 수, 파일 구조체 포인터)
+	// fread_s(저장할 버퍼 주소, 버퍼의 크기, 한번에 읽어 들일 바이트 수, 총 읽어 들일 바이트 수, 파일 구조체 포인터)
+	while (1)
+	{
+		FILE* readPtr = NULL;
+		char buffer[100 * 100] = { 0 };
+
+		errno_t err = fopen_s(&readPtr, "box.txt", "r");
+		if (err)
+		{
+			printf("ERROR : Failed to Open File!!!\n\n");
+			return -1;
+		}
+		else
+		{
+			printf("=======Success to open file.=======\n\n");
+		}
+
+		fread_s(buffer, 100 * 100, 1, 100 * 100, readPtr);
+		printf("%s\n", buffer);
+
+		fclose(readPtr);
+
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			break;
+		}
+		Sleep(17);
+		system("cls");
+	}
+	
 #pragma endregion
 
 	return 0;
